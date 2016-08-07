9b0c1cdbd309d9f6fe11687abc2cfdf2f5a6a9d9
Sean Hinds <sthinds144@gmail.com>
2016-08-07 11:07:33 -0400

    commit_080616-2

--- DNATranscriber.py
+++ DNATranscriber.py
@@ -1,1 +1,14 @@
 # DNA transcriber
+
+import sys
+from sys import argv
+script, input = argv
+
+class Transcriber(object):
+
+	def transcribe(self, DNA):
+		DNA = DNA.replace('T', 'U')
+		print DNA
+		
+t = Transcriber()
+t.transcribe(input)
