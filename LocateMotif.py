9aef6cd0b62aad903b4c8adcb0b9ce5722e0fd3f
Sean Hinds <sthinds144@gmail.com>
2016-08-13 13:02:22 -0400

    commit_081316

---
+++ LocateMotif.py
@@ -0,0 +1,1 @@
+# locate input motif
--- Translate.py
+++ Translate.py
@@ -1,26 +1,42 @@
 import sys
 from sys import argv
+script, input = argv
 
 def translate(string):
-	codons = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
-    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
-    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
-    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
-    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
-    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
-    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
-    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
-    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
-    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
-    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
-    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
-    start = string.find('AUG')
-    while start + 2 < len(string):
-    	codon = string(start:start+2)
-    	if codon == 'UAG': break
-    	print codons[codon]
-    	start += 3    	
+	
+	codons = {
+		"UUU":"F", "UUC":"F", "UUA":"L", 
+		"UUG":"L", "UCU":"S", "UCC":"s", 
+		"UCA":"S", "UCG":"S", "UAU":"Y", 
+		"UAC":"Y", "UAA":"-", "UAG":"-",
+   		"UGU":"C", "UGC":"C", "UGA":"-", 
+   		"UGG":"W", "CUU":"L", "CUC":"L", 
+   		"CUA":"L", "CUG":"L", "CCU":"P", 
+   		"CCC":"P", "CCA":"P", "CCG":"P",
+   		"CAU":"H", "CAC":"H", "CAA":"Q", 
+   		"CAG":"Q", "CGU":"R", "CGC":"R", 
+   		"CGA":"R", "CGG":"R", "AUU":"I", 
+   		"AUC":"I", "AUA":"I", "AUG":"M",
+   		"ACU":"T", "ACC":"T", "ACA":"T", 
+   		"ACG":"T", "AAU":"N", "AAC":"N", 
+   		"AAA":"K", "AAG":"K", "AGU":"S", 
+   		"AGC":"S", "AGA":"R", "AGG":"R",
+   		"GUU":"V", "GUC":"V", "GUA":"V", 
+   		"GUG":"V", "GCU":"A", "GCC":"A", 
+   		"GCA":"A", "GCG":"A", "GAU":"D", 
+   		"GAC":"D", "GAA":"E", "GAG":"E",
+   		"GGU":"G", "GGC":"G", "GGA":"G", 
+   		"GGG":"G",
+	}
+   	
+   	string = string.upper()
+   	start = string.find('AUG')
+   	peptide = []
+   	while start + 2 < len(string):
+   		codon = string[start:start+3]
+   		if codon == 'UAG':
+   			break
+   		peptide.append(codons[codon])
+   		start += 3
+  	print ''.join(peptide)	
+translate(input)
