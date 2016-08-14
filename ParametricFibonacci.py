080507fd3f135c8c598def89ef88be94fe01ed6f
Sean Hinds <sthinds144@gmail.com>
2016-08-13 20:24:56 -0400

    commit_081316

--- ParametricFibonacci.py
+++ ParametricFibonacci.py
@@ -1,1 +1,19 @@
-# Fibonacci sequence. Population n with k offspring per litter
+# Fibonacci sequence. Population after n generations with k offspring per litter
+import sys
+from sys import argv
+script, n, k = argv
+n, k = int(n), int(k)
+
+class paramFibo(object):
+	
+	def population(self, t, m):
+		if t < 2:
+			return t
+		else:
+			return (
+				self.population(t-1, m) +
+				self.population(t-2, m) * m
+			)
+		
+x = paramFibo()
+print x.population(n, k)
--- ReverseComplement.py
+++ ReverseComplement.py
@@ -1,1 +1,24 @@
 # Reverse Complement
+import sys
+from sys import argv
+script, seq = argv
+
+seq = str(seq)
+
+class rComp(object):
+	
+	def __init__(self, string):
+		self.string = string
+		string = string[::-1].upper()
+		pairs = {
+			'A':'T', 'T':'A',
+			'G':'C', 'C':'G'
+		}
+		l = list(string)
+		n = []
+		for char in l:
+			n.append(pairs[char])
+		print ''.join(n)
+		
+rComp(seq)
+
